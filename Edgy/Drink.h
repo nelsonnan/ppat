48904db59b4d31ba50f33a354ea3dc1a18156692
User Interface Design Group <uid@Users-MacBook-Air.local>
2012-11-28 15:20:25 -0500

    commit drink file

---
+++ Edgy/Drink.h
@@ -0,0 +1,13 @@
+//
+//  Drink.h
+//  ImageProcessing
+//
+//  Created by User Interface Design Group on 11/28/12.
+//  Copyright (c) 2012 Lindsay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Drink : NSObject
+
+@end
---
+++ Edgy/Drink.m
@@ -0,0 +1,13 @@
+//
+//  Drink.m
+//  ImageProcessing
+//
+//  Created by User Interface Design Group on 11/28/12.
+//  Copyright (c) 2012 Lindsay. All rights reserved.
+//
+
+#import "Drink.h"
+
+@implementation Drink
+
+@end
